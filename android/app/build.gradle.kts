9d37d058904a4bdbc8b1fdc3d1fb09e1b932f3aa
HusainD4 <mysens.id@gmail.com>
2026-06-06 01:07:31 +0700

    update kompass

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         
-        minSdk = 21 // <-- CUKUP GUNAKAN BARIS INI SAJA
+        minSdk = flutter.minSdkVersion // <-- CUKUP GUNAKAN BARIS INI SAJA
         
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
